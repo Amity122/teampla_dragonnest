431cb12f90da94951d9494df1199fa88b74d4ef2
Amity122 <amityelmwood@gmail.com>
2026-04-05 10:15:56 +0800

    added shift compatibility team formation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Literal, Optional
+from typing import Literal, Optional, List, Dict
 
 ROLES = Literal["DPS", "Healer", "Tank"]
 SHIFTS = Literal["Morning Shift", "Afternoon Shift", "Evening Shift"]
@@ -30,20 +30,59 @@ class Member:
         self.characters.append(character)
         character.member = self
 
+    def __repr__(self):
+        return f"Member('{self.discord_name}', {self.schedule}, {self.characters})"
 class NormalParty:
-    pass
+    
+    def __init__(self):
+        self.members = []
+        self.length = 4
 
+    def form_party(self, members: List[Member]):
+        result = {"Morning Shift": [], "Afternoon Shift": [], "Evening Shift": []}
+        for member in members:
+            if member.schedule.shift  == "Morning Shift" and len(result["Morning Shift"]) < self.length:
+                result["Morning Shift"].append(member)
+            elif member.schedule.shift == "Afternoon Shift" and len(result["Afternoon Shift"]) < self.length:
+                result["Afternoon Shift"].append(member)
+            elif member.schedule.shift == "Evening Shift" and len(result["Evening Shift"]) < self.length:
+                result["Evening Shift"].append(member)
+        self.members = result
+        return result
+    
 class RaidParty:
     pass
 
 def main():
     morning_schedule = Schedule("Morning Shift")
+    afternoon_schedule = Schedule("Afternoon Shift")
+    evening_schedule = Schedule("Evening Shift")
+
     member1 = Member("Amity", morning_schedule)
     character1 = Character(ingame_name="Alysedryn", role="DPS")
     member1.add_character(character1)
+    
+    memberr1 = Member("Yarborough", morning_schedule)
+    characterr1 = Character(ingame_name="Yarborough", role="Healer")
+    memberr1.add_character(characterr1)
+    
+    member2 = Member("Basil", afternoon_schedule)
+    character2 = Character(ingame_name="Rynn", role="Healer")
+    member2.add_character(character2)
+
+    member3 = Member("Cass", evening_schedule)
+    character3 = Character(ingame_name="Torin", role="Tank")
+    member3.add_character(character3)
+
+    all_members = [member1, member2, member3, memberr1]
+
     print(member1.discord_name)  # Output: Amity
     print(member1.characters)
     print(member1.schedule)
-    
+
+    party = NormalParty()
+    party.form_party(all_members)
+    print(party.members)
+
 if __name__ == "__main__":
     main()
