9a679cb3b56c4b01bc4c101ff8c952e8e4f64bf6
Amity122 <amityelmwood@gmail.com>
2026-04-05 09:12:40 +0800

    Added class structures

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+
+
+class Schedule:
+    
+    def __init__(self, shift: str):
+        self.shift = shift
+
+
+class Member:
+    pass
+
+class Characters:
+    pass
+
+class NormalParty:
+    pass
+
+class RaidParty:
+    pass
+
 def main():
-    print("Hello from teampla-dragonnest!")
-
-
+    example_schedule = Schedule("Morning Shift")
+    print(f"Schedule created with shift: {example_schedule.shift}")
+    
 if __name__ == "__main__":
     main()
